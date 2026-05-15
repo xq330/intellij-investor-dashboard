0ab0347cb137b05d8499debfbdd1c67fc252ec6d
徐强 <xq330@qq.com>
2026-05-15 14:58:59 +0800

    更新

--- src/main/kotlin/com/vermouthx/stocker/utils/StockerQuoteParser.kt
+++ src/main/kotlin/com/vermouthx/stocker/utils/StockerQuoteParser.kt
@@ -55,6 +55,83 @@ object StockerQuoteParser {
                     )
                 }
 
+                StockerMarketType.HKStocks -> {
+                    val code = textArray[0].substring(2).uppercase()
+                    val name = textArray[2]
+                    val opening = textArray[3].toDouble()
+                    val close = textArray[4].toDouble()
+                    val high = textArray[5].toDouble()
+                    val low = textArray[6].toDouble()
+                    val current = textArray[7].toDouble()
+                    val change = (current - close).twoDigits()
+                    val percentage = textArray[9].toDouble().twoDigits()
+                    val sourceFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm")
+                    val targetFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                    val datetime = LocalDateTime.parse(textArray[18] + " " + textArray[19], sourceFormatter)
+                    val updateAt = targetFormatter.format(datetime)
+                    StockerQuote(
+                        code = code,
+                        name = name,
+                        current = current,
+                        opening = opening,
+                        close = close,
+                        low = low,
+                        high = high,
+                        change = change,
+                        percentage = percentage,
+                        updateAt = updateAt
+                    )
+                }
+
+                StockerMarketType.USStocks -> {
+                    val code = textArray[0].substring(3).uppercase()
+                    val name = textArray[1]
+                    val current = textArray[2].toDouble()
+                    val updateAt = textArray[4]
+                    val opening = textArray[6].toDouble()
+                    val high = textArray[7].toDouble()
+                    val low = textArray[8].toDouble()
+                    val close = textArray[27].toDouble()
+                    val change = (current - close).twoDigits()
+                    val percentage = textArray[3].toDouble().twoDigits()
+                    StockerQuote(
+                        code = code,
+                        name = name,
+                        current = current,
+                        opening = opening,
+                        close = close,
+                        low = low,
+                        high = high,
+                        change = change,
+                        percentage = percentage,
+                        updateAt = updateAt
+                    )
+                }
+
+                StockerMarketType.Crypto -> {
+                    val code = textArray[0].substring(4).uppercase()
+                    val name = textArray[10]
+                    val current = textArray[9].toDouble()
+                    val low = textArray[8].toDouble()
+                    val high = textArray[7].toDouble()
+                    val opening = textArray[6].toDouble()
+                    val change = (current - opening).twoDigits()
+                    val percentage = ((current - opening) / opening * 100).twoDigits()
+                    val updateAt = "${textArray[12]} ${textArray[1]}"
+                    StockerQuote(
+                        code = code,
+                        name = name,
+                        current = current,
+                        opening = opening,
+                        close = current,
+                        low = low,
+                        high = high,
+                        change = change,
+                        percentage = percentage,
+                        updateAt = updateAt
+                    )
+                }
+
                 StockerMarketType.QH -> {
                     val code = textArray[0].substring(3).uppercase()
                     val name = textArray[1]

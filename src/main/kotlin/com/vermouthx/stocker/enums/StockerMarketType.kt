3d3a4da48ba125cb67a32e8285832f43e8646376
徐强 <xq330@qq.com>
2026-05-15 14:44:07 +0800

    更新

--- src/main/kotlin/com/vermouthx/stocker/enums/StockerMarketType.kt
+++ src/main/kotlin/com/vermouthx/stocker/enums/StockerMarketType.kt
@@ -2,5 +2,8 @@ package com.vermouthx.stocker.enums
 
 enum class StockerMarketType(val title: String) {
     AShare("CN"),
+    HKStocks("HK"),
+    USStocks("US"),
+    Crypto("Crypto"),
     QH("QH")
 }
